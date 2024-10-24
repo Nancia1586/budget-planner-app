48651b8eb72929269247d292676401651b88f56d
Nancia Rajerison <lalaina.nancia64@gmail.com>
2024-10-24 23:02:58 +0300

    init project and login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import { LoginComponent } from './features/auth/login/login.component';
+// import { SignupComponent } from './features/auth/signup/signup.component';
+// import { ExpensesComponent } from './features/expenses/expenses.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  // { path: 'signup', component: SignupComponent },
+  // { path: 'expenses', component: ExpensesComponent },
+];
